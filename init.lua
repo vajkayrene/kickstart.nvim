6220de76c98d74cd01bef7389a02c04df6f1d9d3
Vajkay René <rene.vajkay@protonmail.com>
2026-05-22 20:36:11 +0200

    Replace bufferline.nvim with barbar.nvim

--- init.lua
+++ init.lua
@@ -980,12 +980,19 @@ require('lazy').setup({
   },
 
   {
-    'akinsho/bufferline.nvim',
-    version = '*',
-    dependencies = 'nvim-tree/nvim-web-devicons',
-    config = function()
-      require('bufferline').setup()
-    end,
+    'romgrk/barbar.nvim',
+    dependencies = {
+      'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
+      'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
+    },
+    init = function() vim.g.barbar_auto_setup = false end,
+    opts = {
+      -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
+      -- animation = true,
+      -- insert_at_start = true,
+      -- …etc.
+    },
+    version = '^1.0.0', -- optional: only update when a new 1.x version is released
   },
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the

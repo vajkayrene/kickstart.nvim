94735135912275b0b4d27b7aff092b0bb79af8ea
Vajkay René <rene.vajkay@protonmail.com>
2026-05-22 20:22:52 +0200

    Fix missing comma in init.lua

--- init.lua
+++ init.lua
@@ -986,7 +986,7 @@ require('lazy').setup({
     config = function()
       require('bufferline').setup()
     end,
-  }
+  },
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and

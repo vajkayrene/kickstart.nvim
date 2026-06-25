f6bf02a4193e64eed13a0a5ea90fe3d9518c357f
Vajkay René <rene.vajkay@protonmail.com>
2026-06-25 10:13:17 +0200

    Reconfigure Neotree behaviour

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -11,9 +11,9 @@ return {
     'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
     'MunifTanjim/nui.nvim',
   },
-  lazy = false,
   keys = {
     { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+    { '<leader>e', ':Neotree current', desc = 'Neotree fullscreen', silent = true },
   },
   opts = {
     event_handlers = {

b23e577da6cd1c163ecc2a0d0d996bc36debb0d6
Vajkay René <rene.vajkay@protonmail.com>
2026-06-26 09:58:53 +0200

    Add json-nvim plugin

--- init.lua
+++ init.lua
@@ -934,6 +934,11 @@ require('lazy').setup({
     opts = {},
   },
 
+  {
+    "VPavliashvili/json-nvim",
+    ft = "json", -- only load for json filetype
+  },
+
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
